c8646d0df2819271847d6362b5d9f85a5315d432
kyangah <69961793+kyangah@users.noreply.github.com>
2020-12-02 20:35:31 +0900

    Update product_basic.js

--- js/product_basic.js
+++ js/product_basic.js
@@ -312,7 +312,7 @@ function maskOut(){
 function videoRandomChange(){
     var randomNum = Math.floor(Math.random() * 3 + 1); // 랜덤 값 생성 후 자연수로 변경
     console.log(randomNum);
-    document.getElementById('ranvedio').src="img/index/menu0" + randomNum + ".avi";
+    document.getElementById('ranvedio').src="img/index/menu0" + randomNum + ".webm";
 }
 //  var iiii = 0;
 //  function imgChange() {
